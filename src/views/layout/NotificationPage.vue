ccf0f2cfb907d6d13fcc5893e24ad1bd24bd2163
Eddie <Eddie_yang7@163.com>
2024-12-12 22:01:22 +0800

    给通知页添加了tab栏

--- src/views/layout/NotificationPage.vue
+++ src/views/layout/NotificationPage.vue
@@ -1,3 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+//定义导航栏数据
+const channels = [
+  '评论和@',
+  '赞和收藏',
+  '新增关注'
+]
+const currentChannel = ref('评论和@')
+
+//切换channel方法
+const switchChannel = (nav) => {
+  currentChannel.value = nav
+}
+
+</script>
+
 <template>
-  <div style="font-size: 100px;">通知夜夜夜夜</div>
+  <div>
+    <div class="channel">
+      <button
+        v-for="(channel, index) in channels"
+        :key="index"
+        :class="{ active: currentChannel === channel }"
+        @click="switchChannel(channel)"
+      >
+        {{ channel }}
+      </button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+button {
+  font-size: 15px;
+  height: 40px;
+  padding: 10px 20px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  border-radius: 20px;
+  background-color: #fff;
+}
+button:hover {
+  background-color: #f7f7f7;
+}
+.active {
+  background-color: #f7f7f7;
+  font-weight: bold;
+}
+</style>
